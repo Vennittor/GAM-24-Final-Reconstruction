2853cf0af611e9e909f2e2453179202473bb4ffc
Vennittor <ventor_prime@hotmail.com>
2015-12-06 22:48:31 -0800

    AI

---
+++ Assets/Enemy AI/Enemy.cs
@@ -0,0 +1,225 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Enemy : MonoBehaviour 
+{
+	public List<EnemyState> states = new List<EnemyState>();
+	public StateMachineEngine stateMachine = null;
+	Collider[] possibleTargets = null;
+	float distance = 0.0f;
+	int randomExclusiveIndex = 0;
+	int randomConcurrentIndex = 0;
+
+	EnemyState[] exclusiveStates = {EnemyState.NONE, EnemyState.PRESSINGA, EnemyState.PRESSINGB, 
+									EnemyState.PRESSINGX, EnemyState.PRESSINGLEFTBUMPER, 
+									EnemyState.PRESSINGRIGHTBUMPER, EnemyState.PRESSINGLEFTTRIGGER,
+									EnemyState.PRESSINGRIGHTTRIGGER};
+
+	EnemyState[] concurrentStates = {EnemyState.MOVEHORIZONTAL, EnemyState.MOVEVERTICAL};
+	
+	void OnDrawGizmos()
+	{
+		Gizmos.color = Color.yellow;
+		Gizmos.DrawSphere (this.transform.position, 10f);
+	}
+
+	void Awake()
+	{
+		stateMachine.Initialize<EnemyState> (this);
+		stateMachine.ChangeState (EnemyState.NONE);
+	}
+	
+	void Start () 
+	{
+		StartCoroutine(ChooseExclusiveIndex(2.0f));
+		StartCoroutine(ChooseConcurrentIndex(0.5f));
+	}
+
+	void FixedUpdate()
+	{
+		possibleTargets = Physics.OverlapSphere (this.transform.position, 10f);
+		for (int i = 0; i < possibleTargets.Length; i++) 
+		{
+			distance = Vector3.Distance(this.transform.position, possibleTargets[i].transform.position);
+			if (distance >= 5f)
+			{
+				stateMachine.AddConcurrentState(concurrentStates[randomConcurrentIndex]);
+			}
+			if (distance <= 5.1f)
+			{
+				stateMachine.ChangeState(exclusiveStates[randomExclusiveIndex]);
+			}
+		}
+	}
+
+	void Update () 
+	{
+		states = stateMachine.GetStates<EnemyState> ();
+	}
+
+	IEnumerator ChooseExclusiveIndex (float frequency)
+	{
+		randomExclusiveIndex = Random.Range (0, exclusiveStates.Length);
+		yield return new WaitForSeconds (frequency);
+		StartCoroutine (ChooseExclusiveIndex (frequency));
+	}
+
+	IEnumerator ChooseConcurrentIndex (float frequency)
+	{
+		randomConcurrentIndex = Random.Range (0, concurrentStates.Length);
+		yield return new WaitForSeconds (frequency);
+		StartCoroutine (ChooseConcurrentIndex (frequency));
+	}
+
+	#region NONE
+	public void NONE_Enter()
+	{
+
+	}
+	public void NONE_Update()
+	{
+
+	}
+	public void NONE_Exit ()
+	{
+
+	}
+	#endregion
+
+	#region Pressing A
+	public void PRESSINGA_Enter()
+	{
+		print ("Computer pressed A!");
+	}
+	public void PRESSINGA_Update()
+	{
+		//*CALL INPUT*//
+	}
+	public void PRESSINGA_Exit ()
+	{
+		
+	}
+	#endregion
+
+	#region Pressing B
+	public void PRESSINGB_Enter()
+	{
+		print ("Computer pressed B!");
+	}
+	public void PRESSINGB_Update()
+	{
+		//*CALL INPUT*//
+	}
+	public void PRESSINGB_Exit ()
+	{
+		
+	}
+	#endregion
+
+	#region PRESSING_X
+	public void PRESSINGX_Enter()
+	{
+		print ("Computer pressed X!");
+	}
+	public void PRESSINGX_Update()
+	{
+		//*CALL INPUT*//
+	}
+	public void PRESSINGX_Exit ()
+	{
+		
+	}
+	#endregion
+
+	#region PRESSING_LEFTBUMPER
+	public void PRESSINGLEFTBUMPER_Enter()
+	{
+		print ("Computer pressed Left Bumper!");
+	}
+	public void PRESSINGLEFTBUMPER_Update()
+	{
+		//*CALL INPUT*//
+	}
+	public void PRESSINGLEFTBUMPER_Exit ()
+	{
+		
+	}
+	#endregion
+
+	#region PRESSING_RIGHTBUMPER
+	public void PRESSINGRIGHTBUMPER_Enter()
+	{
+		print ("Computer pressed Right Bumper!");
+	}
+	public void PRESSINGRIGHTBUMPER_Update()
+	{
+		//*CALL INPUT*//
+	}
+	public void PRESSINGRIGHTBUMPER_Exit ()
+	{
+		
+	}
+	#endregion
+
+	#region PRESSING_LEFTTRIGGER
+	public void PRESSINGLEFTTRIGGER_Enter()
+	{
+		print ("Computer pressed Left Trigger!");
+	}
+	public void PRESSINGLEFTTRIGGER_Update()
+	{
+		//*CALL INPUT*//
+	}
+	public void PRESSINGLEFTTRIGGER_Exit ()
+	{
+		
+	}
+	#endregion
+
+	#region PRESSING_RIGHTTRIGGER
+	public void PRESSINGRIGHTTRIGGER_Enter()
+	{
+		print ("Computer pressed Right Trigger!");
+	}
+	public void PRESSINGRIGHTTRIGGER_Update()
+	{
+		//*CALL INPUT*//
+	}
+	public void PRESSINGRIGHTTRIGGER_Exit ()
+	{
+		
+	}
+	#endregion
+
+	#region MOVE_HORIZONTAL
+	public void MOVEHORIZONTAL_Enter()
+	{
+		print ("Computer is moving horizontally!");
+	}
+	public void MOVEHORIZONTAL_Update()
+	{
+		//*CALL INPUT*//
+	}
+	public void MOVEHORIZONTAL_Exit ()
+	{
+		
+	}
+	#endregion
+
+	#region MOVE_VERTICAL
+	public void MOVEVERTICAL_Enter()
+	{
+		print ("Computer is moving vertically!");
+	}
+	public void MOVEVERTICAL_Update()
+	{
+		//*CALL INPUT*//
+	}
+	public void MOVEVERTICAL_Exit ()
+	{
+		
+	}
+	#endregion
+
+}
---
+++ Assets/Enemy AI/StateMachineEngine.cs
@@ -0,0 +1,362 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+using Object = System.Object;
+
+public class StateMachineEngine : MonoBehaviour
+{
+	private List<StateMapping> currentStates = new List<StateMapping>();
+	
+	private Dictionary<Enum, StateMapping> stateLookup;
+	private Dictionary<string, Delegate> methodLookup;
+	
+	private readonly string[] ignoredNames = new[] { "add", "remove", "get", "set" };
+	
+	private bool isInTransition = false;
+	
+	public void Initialize<T>(MonoBehaviour entity)
+	{
+		//Define States
+		var values = Enum.GetValues(typeof(T));
+		stateLookup = new Dictionary<Enum, StateMapping>();
+		for (int i = 0; i < values.Length; i++)
+		{
+			var mapping = new StateMapping((Enum) values.GetValue(i));
+			stateLookup.Add(mapping.state, mapping);
+		}
+		
+		//Reflect methods
+		var methods = entity.GetType().GetMethods(BindingFlags.Instance | BindingFlags.DeclaredOnly | BindingFlags.Public |
+		                                          BindingFlags.NonPublic);
+		
+		//Bind methods to states
+		var separator = "_".ToCharArray();
+		for (int i = 0; i < methods.Length; i++)
+		{
+			var names = methods[i].Name.Split(separator);
+			
+			//Ignore functions without an underscore
+			if (names.Length <= 1)
+			{
+				continue;
+			}
+			
+			Enum key;
+			try
+			{
+				key = (Enum) Enum.Parse(typeof(T), names[0]);
+			}
+			catch (ArgumentException)
+			{
+				//Some things (evetns, properties) generate automatic method. Ignore these
+				for (int j = 0; j < ignoredNames.Length; j++)
+				{
+					if (names[0] == ignoredNames[j])
+					{
+						goto SkipWarning;
+					}
+				}
+				
+				Debug.LogWarning("Method with name " + methods[i].Name + " could not resolve a matching state. Check method spelling");
+				continue;
+				
+			SkipWarning:
+					continue;
+			}
+			
+			var targetState = stateLookup[key];
+			
+			switch (names[1])
+			{
+			case "Enter":
+				if (methods[i].ReturnType == typeof(IEnumerator))
+				{
+					targetState.Enter = CreateDelegate<Func<IEnumerator>>(methods[i], entity);
+				}
+				else
+				{
+					var action = CreateDelegate<Action>(methods[i], entity);
+					targetState.Enter = () => { action(); return null; };
+				}
+				break;
+			case "Exit":
+				if (methods[i].ReturnType == typeof(IEnumerator))
+				{
+					targetState.Exit = CreateDelegate<Func<IEnumerator>>(methods[i], entity);
+				}
+				else
+				{
+					var action = CreateDelegate<Action>(methods[i], entity);
+					targetState.Exit = () => { action(); return null; };
+				}
+				break;
+			case "Update":
+				targetState.Update = CreateDelegate<Action>(methods[i], entity);
+				break;
+			case "LateUpdate":
+				targetState.LateUpdate = CreateDelegate<Action>(methods[i], entity);
+				break;
+			case "FixedUpdate":
+				targetState.FixedUpdate = CreateDelegate<Action>(methods[i], entity);
+				break;
+			}
+		}
+	}
+	
+	private V CreateDelegate<V>(MethodInfo method, Object target) where V : class
+	{
+		var ret = (Delegate.CreateDelegate(typeof (V), target, method) as V);
+		
+		if (ret == null)
+		{
+			throw new ArgumentException("Unabled to create delegate for method called " + method.Name);
+		}
+		return ret;
+		
+	}
+
+	public void AddConcurrentState(Enum newState)
+	{
+		if (stateLookup == null)
+		{
+			throw new Exception("States have not been configured, please call initialized before trying to set state");
+		}
+		
+		if (!stateLookup.ContainsKey(newState))
+		{
+			throw new Exception("No state with the name " + newState.ToString() + " can be found. Please make sure you are called the correct type the statemachine was initialized with");
+		}
+
+		var nextState = stateLookup[newState];
+
+		foreach (StateMapping currentState in currentStates)
+		{
+			if (currentState == nextState) return;
+		}
+		if (gameObject.activeInHierarchy == true)
+		{
+			StartCoroutine(AddConcurrentStateRoutine(nextState));
+		}
+	}
+
+	public void RemoveConcurrentState(Enum newState)
+	{
+		if (stateLookup == null)
+		{
+			throw new Exception("States have not been configured, please call initialized before trying to set state");
+		}
+		
+		if (!stateLookup.ContainsKey(newState))
+		{
+			throw new Exception("No state with the name " + newState.ToString() + " can be found. Please make sure you are called the correct type the statemachine was initialized with");
+		}
+
+		var nextState = stateLookup[newState];
+
+		foreach (StateMapping currentState in currentStates)
+		{
+			if (currentState == nextState)
+			{
+				if (gameObject.activeInHierarchy == true)
+				{
+					StartCoroutine(RemoveConcurrentStateRoutine(nextState));
+					return;
+				}
+			}
+		}
+	}
+	
+	public void ChangeState(Enum newState)
+	{
+		if (stateLookup == null)
+		{
+			throw new Exception("States have not been configured, please call initialized before trying to set state");
+		}
+		
+		if (!stateLookup.ContainsKey(newState))
+		{
+			throw new Exception("No state with the name " + newState.ToString() + " can be found. Please make sure you are called the correct type the statemachine was initialized with");
+		}
+		
+		var nextState = stateLookup[newState];
+
+		foreach (StateMapping currentState in currentStates)
+		{
+			if (currentState == nextState) return;
+		}
+		if (gameObject.activeInHierarchy == true)
+		{
+			StartCoroutine(ChangeToNewStateRoutine(nextState));
+		}
+	}
+
+	private IEnumerator AddConcurrentStateRoutine(StateMapping newState)
+	{
+		currentStates.Add(newState);
+		
+		if (newState != null)
+		{
+			var enterRoutine = newState.Enter();
+			
+			if (enterRoutine != null)
+			{
+				yield return StartCoroutine(enterRoutine);
+			}
+		}
+	}
+
+	private IEnumerator RemoveConcurrentStateRoutine(StateMapping newState)
+	{
+		currentStates.Remove(newState);
+		if (newState != null)
+		{
+			var exitRoutine = newState.Exit();
+			
+			if (exitRoutine != null)
+			{
+				yield return StartCoroutine(exitRoutine);
+			}
+		}
+	}
+	
+	private IEnumerator ChangeToNewStateRoutine(StateMapping newState)
+	{
+		isInTransition = true;
+
+		foreach (StateMapping currentState in currentStates)
+		{
+			if (currentState != null)
+			{
+				
+				var exitRoutine = currentState.Exit();
+				
+				if (exitRoutine != null)
+				{
+					yield return StartCoroutine(exitRoutine);
+				}
+			}
+		}
+
+		currentStates = new List<StateMapping>();
+		currentStates.Add(newState);
+		
+		if (newState != null)
+		{
+			var enterRoutine = newState.Enter();
+			
+			if (enterRoutine != null)
+			{
+				yield return StartCoroutine(enterRoutine);
+			}
+		}
+		
+		isInTransition = false;
+	}
+	
+	void FixedUpdate()
+	{
+		foreach (StateMapping currentState in currentStates)
+		{
+			if (currentState != null)
+			{
+				currentState.FixedUpdate();
+			}
+		}
+	}
+	
+	void Update()
+	{
+		for (int i = 0; i < currentStates.Count; i++)
+		{
+			if (currentStates[i] != null && !isInTransition)
+			{
+				currentStates[i].Update();
+			}
+		}
+	}
+	
+	void LateUpdate()
+	{
+		foreach (StateMapping currentState in currentStates)
+		{
+			if (currentState != null && !isInTransition)
+			{
+				currentState.LateUpdate();
+			}
+		}
+	}
+	
+	public static void DoNothing()
+	{
+	}
+	public static void DoNothingCollider(Collider other)
+	{
+	}
+	public static void DoNothingCollision(Collision other)
+	{
+	}
+	public static IEnumerator DoNothingCoroutine()
+	{
+		yield break;
+	}
+	
+	public Enum GetState()
+	{
+		foreach (StateMapping currentState in currentStates)
+		{
+			if (currentState != null)
+			{
+				return currentState.state;
+			}
+		}
+		return null;
+	}
+
+	public bool GetState<T> (T state)
+	{
+		foreach (StateMapping currentState in currentStates)
+		{
+			if (((T)(object)currentState.state).Equals(state))
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public List<T> GetStates<T>()
+	{
+		List<T> returnStates = new List<T>();
+		foreach (StateMapping currentState in currentStates)
+		{
+			if (currentState != null)
+			{
+				returnStates.Add((T)(object)currentState.state);
+			}
+		}
+		return returnStates;
+	}
+}
+
+
+
+public class StateMapping
+{
+	public Enum state;
+	
+	public Func<IEnumerator> Enter = StateMachineEngine.DoNothingCoroutine;
+	public Func<IEnumerator> Exit = StateMachineEngine.DoNothingCoroutine;
+	public Action Update = StateMachineEngine.DoNothing;
+	public Action LateUpdate = StateMachineEngine.DoNothing;
+	public Action FixedUpdate = StateMachineEngine.DoNothing;
+	
+	public StateMapping(Enum state)
+	{
+		this.state = state;
+	}
+	
+}
+
+
