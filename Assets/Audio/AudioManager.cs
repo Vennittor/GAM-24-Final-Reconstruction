d4fd1934a00cb1e45e99e9bffccb085e784f2fac
Vennittor <ventor_prime@hotmail.com>
2015-12-08 13:54:44 -0800

    Added Audio Manager

---
+++ Assets/Audio/AudioManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioManager : MonoBehaviour 
+{
+	public AudioSource sfx;
+	public static AudioManager instance = null;
+	public GameObject prefab = null;
+
+	private GameObject go = null;
+
+	void Awake ()
+	{
+		if (instance == null)
+			instance = this;
+		else if (instance != this)
+			Destroy (gameObject);
+
+		DontDestroyOnLoad (gameObject);
+	}
+
+	void PlaySound (AudioClip clip)
+	{
+		go = Instantiate (prefab);
+		sfx = go.GetComponent<AudioSource> ();
+		sfx.clip = clip;
+		sfx.Play ();
+
+		Destroy (sfx, clip.length);
+	}
+}
