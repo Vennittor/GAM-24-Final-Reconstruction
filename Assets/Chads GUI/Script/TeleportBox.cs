1e1fa6c1a6172133f933011b57a777b1b738dafc
ocknon <ockn0n@hotmail.com>
2015-12-11 05:14:56 -0800

    Fixed errors

--- Assets/Chads GUI/Script/TeleportBox.cs
+++ Assets/Chads GUI/Script/TeleportBox.cs
@@ -24,26 +24,29 @@ public class TeleportBox : MonoBehaviour
 
     void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.GetComponent<BaseCharacter>())
-        {
-            other.gameObject.GetComponent<BaseCharacter>().lives--;
-            other.gameObject.GetComponent<BaseCharacter>().health = 0;
-            GameObject sceneCam = GameObject.Find("SceneCamera");
-            sceneCam.GetComponent<SceneCamera>().players.Remove(other.gameObject);
-            if (other.gameObject.GetComponent<BaseCharacter>().lives > 0)
-            {
-                Debug.Log("respawn");
-                other.GetComponent<Rigidbody>().velocity = Vector3.zero;
-                other.transform.position = deathSpawnPoint.transform.position;
-                other.GetComponent<BaseCharacter>().Respawn(true);
-                
-                sceneCam.GetComponent<SceneCamera>().players.Add(other.gameObject);
-            }
-            else
-            {
-                other.gameObject.GetComponent<DisabledStates>().LockAll();
-                other.gameObject.GetComponent<Rigidbody>().isKinematic = true;
-            }
-        }
+		if (other.gameObject.tag == "Player")
+		{
+	        if (other.gameObject.GetComponent<BaseCharacter>())
+	        {
+	            other.gameObject.GetComponent<BaseCharacter>().lives--;
+	            other.gameObject.GetComponent<BaseCharacter>().health = 0;
+	            GameObject sceneCam = GameObject.Find("SceneCamera");
+	            sceneCam.GetComponent<SceneCamera>().players.Remove(other.gameObject);
+	            if (other.gameObject.GetComponent<BaseCharacter>().lives > 0)
+	            {
+	                Debug.Log("respawn");
+	                other.GetComponent<Rigidbody>().velocity = Vector3.zero;
+	                other.transform.position = deathSpawnPoint.transform.position;
+	                other.GetComponent<BaseCharacter>().Respawn(true);
+	                
+	                sceneCam.GetComponent<SceneCamera>().players.Add(other.gameObject);
+	            }
+	            else
+	            {
+	                other.gameObject.GetComponent<DisabledStates>().LockAll();
+	                other.gameObject.GetComponent<Rigidbody>().isKinematic = true;
+	            }
+	        }
+		}
     }
 }
--- Assets/TvCam.cs
+++ Assets/TvCam.cs
@@ -3,28 +3,28 @@ using System.Collections;
 
 public class TvCam : MonoBehaviour
 {
-    GameObject sceneCam, player = null;
-    SceneCamera camScript;
-	// Use this for initialization
-	void Start ()
-    {
-        sceneCam = GameObject.Find("SceneCamera");
-        camScript = sceneCam.GetComponent<SceneCamera>();
-         
-	}
-	
-	// Update is called once per frame
-	void Update ()
-    {
-        if (player == null)
-        {
-            if (camScript.players.Count != 0)
-            {
-                player = camScript.players[Random.Range(0, camScript.players.Count - 1)];
-            }
-        }
-        else
-            transform.position = Vector3.Lerp(transform.position, new Vector3(player.transform.position.x,
-                player.transform.position.y, transform.position.z), 10);
-	}
+//    GameObject sceneCam, player = null;
+//    SceneCamera camScript;
+//	// Use this for initialization
+//	void Start ()
+//    {
+//        sceneCam = GameObject.Find("SceneCamera");
+//        camScript = sceneCam.GetComponent<SceneCamera>();
+//         
+//	}
+//	
+//	// Update is called once per frame
+//	void Update ()
+//    {
+//        if (player == null)
+//        {
+//            if (camScript.players.Count != 0)
+//            {
+//                player = camScript.players[Random.Range(0, camScript.players.Count - 1)];
+//            }
+//        }
+//        else
+//            transform.position = Vector3.Lerp(transform.position, new Vector3(player.transform.position.x,
+//                player.transform.position.y, transform.position.z), 10);
+//	}
 }
